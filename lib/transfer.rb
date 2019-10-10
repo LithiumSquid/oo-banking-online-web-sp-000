f5580ac1d0884cd3a052142a88518b760a4e028a
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-10 23:17:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,11 @@ end
 
 def execute_transaction
   if @sender.balance >= @amount && @status == "pending"
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+  else
+    
 end
 
 def reverse_transfer
