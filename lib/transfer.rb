e217fd2604924856967bfd9dbc3bde9651683938
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-10 23:17:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,10 +24,13 @@ def execute_transaction
     @receiver.balance += @amount
     @status = "complete"
   else
-    
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  end
 end
 
 def reverse_transfer
+  
 end
     
 end
