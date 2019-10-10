787167e32f2c96cf6b38af452614c06fa0c0cfb9
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-10 22:50:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,27 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status
+  attr_reader :amount, :bankaccount
+  
+  
+  def initialize(sender, receiver, amount = 50)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    if bankaccount.valid? == true
+      return true
+    else
+      return false
+    end
+end
+
+def execute_transaction
+end
+
+def reverse_transfer
+end
+    
 end
